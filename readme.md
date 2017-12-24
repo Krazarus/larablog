e9b9801e05a28ba8cd34673245159c5c7410b922
Andrii Shcherbakov <shcherbakov1andrii@gmail.com>
2017-12-24 19:53:36 +0200

    modified readme and web routes

--- readme.md
+++ readme.md
@@ -1,9 +1,8 @@
 1. git clone
 2. connect to db
 3. composer update
-4. mkdir /public/images
-5. php artisan migrate:refresh --seed
-6. phpunit
+4. php artisan migrate:refresh --seed
+5. phpunit
 
 
 admin
--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,6 @@
 |
 */
 
-Route::get('/test', function () {
-
-    $array = [
-        1 => [1, 4, 5, 8, 11, 23, 25, 28, 31, 44, 50],
-        2 => [1, 2, 4, 7, 11, 32, 43, 46, 49],
-        3 => [1, 3, 5, 6, 8, 11, 23, 33, 44, 49],
-        4 => [1],
-        5 => [5],
-        6 => [5],
-        7 => [1],
-        8 => [1],
-        9 => [1],
-        10 => [1]
-
-    ];
-    foreach ($array as $items => $value) {
-        foreach ($value as $item) {
-            echo "factory->create(['user_id' => {$items}, 'liked_id' => {$item}]" . '<br>';
-        }
-    }
-//    dd($array);
-});
-
-
 Auth::routes();
 
 Route::get('/', 'PostsController@index')->name('home');
